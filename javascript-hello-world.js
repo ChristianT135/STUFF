8c176e10f98a3ffdd221922ad64bcc9af83b1be7
Christian Thomas <98788679+ChristianT135@users.noreply.github.com>
2022-02-19 16:10:40 -0500

    Add files via upload

---
+++ javascript-hello-world.js
@@ -0,0 +1,9 @@
+function getName() {
+    const userName = prompt("whats is your name?");
+    sayHello(userName);
+}
+ function sayHello(name) {
+     
+ alert("Hello " + name + "!");
+ console.log ("alert sent!");
+ }
